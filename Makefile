2f8fccf8f7a5eb43aed362d9f53e2a95a2eac3f7
Lydian Lee <tingyenl@yelp.com>
2019-10-27 01:34:23 -0700

    build package

--- Makefile
+++ Makefile
@@ -10,7 +10,11 @@ test:
 develop-server: virtualenv_run
 	JUPYTER_CONFIG_DIR=example/ virtualenv_run/bin/jupyter lab --ip $(shell hostname -f)  -y --no-browser
 
+build:
+	tox -e build
+
 clean:
 	rm -rf virtualenv_run/
 	rm -rf .tox
+	rm -rf dist/
 
--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import unicode_literals
+import os
 
 from setuptools import find_packages
 from setuptools import setup
 
 
-with open("README.md") as fp:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")) as fp:
     long_description = fp.read()
 
 setup(
     name="multicontents",
     version="0.1.0",
     description="providing contents from multiple sources in jupyter notebook",
-    long_description=long_description,
     long_description_content_type="text/markdown",
+    long_description=long_description,
     url="https://github.com/lydian/multicontents",
     author="Lydian Lee",
     author_email="lydianly@gmail.com",
     maintainer="Lydian Lee",
     maintainer_email="lydianly@gmail.com",
-    packages=find_packages(exclude=("tests*",)),
+    packages=find_packages(),
     install_requires=["notebook", "ipykernel"],
     python_requires=">=3.6",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
